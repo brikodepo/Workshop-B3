1564c59cf926f3076a81137a0db4baaee070c24b
TomWilkRavoux <tomwilkwilk@gmail.com>
2024-09-24 14:43:20 +0200

    ajustement map zero

--- workshop_bachelor/src/components/map.jsx
+++ workshop_bachelor/src/components/map.jsx
@@ -1,38 +1,69 @@
-import React from 'react';
-import {MapContainer, ImageOverlay, Marker, Popup} from 'react-leaflet';
-import L from 'leaflet';
-import planZero from '../images/plan_zero_f.png';
-import "../map.style.css"
+import React, { useEffect, useRef } from 'react';
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+import planZero from '../images/plan_zero.png';
+import "../map.style.css";
 
-
-const limite = [[0, 0], [1000, 1000]]; // coordonnees de la map enn fonction de l'image
+const limite = [[0, 0], [375, 1504]]; // Coordonnées de la carte en fonction de l'image
 
 const BuildMap = () => {
+    const mapRef = useRef();
+
+    useEffect(() => {
+        const map = mapRef.current;
+
+        if (map) {
+            // Vérifier le zoom initial et désactiver le dragging si nécessaire
+            const currentZoom = map.getZoom();
+            if (currentZoom === 1) {
+                map.dragging.disable();
+            }
+
+            // Gérer l'activation ou la désactivation du drag au changement de zoom
+            const handleZoomEnd = () => {
+                const currentZoom = map.getZoom();
+                if (currentZoom > 1) {
+                    map.dragging.enable(); // Activer le drag quand le zoom est > 1
+                } else {
+                    map.dragging.disable(); // Désactiver le drag quand le zoom est à 1
+                }
+            };
+
+            map.on('zoomend', handleZoomEnd);
+
+            // Nettoyer l'événement à la désinstallation du composant
+            return () => {
+                map.off('zoomend', handleZoomEnd);
+            };
+        }
+    }, []);
+
     return (
         <MapContainer
-            bounds = {limite}
-            style = {{ height: "100%", width: "100%" }} // a changer le px en vh ou em 
-            zoomControl = {true} // controle du zoom
-            maxZoom = {3}
-            minZoom = {-1}
+            whenCreated={mapInstance => mapRef.current = mapInstance} // Assigner l'instance de la carte à la référence
+            bounds={limite}
+            maxBounds={limite}
+            maxBoundsViscosity={1.0}
+            style={{ height: "100%", width: "100%" }} // 100vh pour occuper toute la hauteur
+            zoomControl={true}
+            zoom={1}
+            maxZoom={3}
+            minZoom={1}
         >
-{/*******************************Chargement du plan image******************************************************/}
+            {/******************************* Chargement du plan image ******************************************************/}
             <ImageOverlay
-                url = {planZero}
-                bounds = {limite} 
+                url={planZero}
+                bounds={limite}
+                className="custom-overlay"
             />
 
-
-
-{/********************************Mise en place des points*****************************************************/}
-            <Marker position={[500, 500]}>
+            {/******************************** Mise en place des points *****************************************************/}
+            <Marker position={[187.5, 752]}>
                 <Popup>
-                    Blablablablabla 
+                    Blablablablabla
                 </Popup>
             </Marker>
-
         </MapContainer>
-    )
+    );
 }
 
 export default BuildMap;
