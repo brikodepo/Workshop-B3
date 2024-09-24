cfc82d8150bcdc953b2b2c2cd7456e49776b7bc9
Cristelle <cristelleal@gmail.com>
2024-09-24 10:07:35 +0200

    add first part of frontend

--- workshop_bachelor/src/App.js
+++ workshop_bachelor/src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Loader from './components/loader';
-import './index.css';
+import Homepage from './components/homepage';
+import Team from './components/team';
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
-    <>
-    <Loader />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Loader />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+      </Routes>
+    </Router>
   );
 }
 
