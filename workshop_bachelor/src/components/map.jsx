8f85580e03688963845aca96b0b2131fe42dc616
TomWilkRavoux <tomwilkwilk@gmail.com>
2024-09-25 09:07:55 +0200

    map zero update

--- workshop_bachelor/src/components/map.jsx
+++ workshop_bachelor/src/components/map.jsx
@@ -1,23 +1,74 @@
-import React, { useEffect, useRef } from 'react';
-import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, ImageOverlay, Marker, Popup, useMap } from 'react-leaflet';
 import planZero from '../images/plan_zero.png';
 import "../map.style.css";
 
 const limite = [[0, 0], [375, 1504]]; // Coordonnées de la carte en fonction de l'image
+const position = [1500, 755];
+
+ {/******************************* gerer le dragger ******************************************************/}
+const ZoomControlDrag = ({ minZoom, maxZoom }) => {
+    const map = useMap();
+
+    useEffect(() => {
+        const handleZoom = () => {
+            const currentZoom = map.getZoom();
+            if (currentZoom > minZoom) {
+                map.dragging.enable();  // Activer le drag si zoom est sup à minZoom
+            } else {
+                map.dragging.disable();  // Désactiver le drag si zoom est égal à minZoom
+            }
+        };
+
+        map.on('zoomend', handleZoom);
+
+        map.dragging.disable();
+
+        return () => {
+            map.off('zoomend', handleZoom);
+        };
+    }, [map, minZoom]);
+
+    return null;
+};
+
+ {/******************************* gerer le repositionement de l'image ******************************************************/}
+const initialZoom = 1; 
+
+    const ResetViewOnZoom = ({ initialCenter, initialZoom }) => {
+        const map = useMap();
+
+        useEffect(() => {
+            const handleZoomEnd = () => {
+                const currentZoom = map.getZoom();
+                if (currentZoom === initialZoom) {
+                    map.setView(initialCenter, initialZoom);  
+            }
+        };
+
+        map.on('zoomend', handleZoomEnd);
+
+        return () => {
+            map.off('zoomend', handleZoomEnd);
+        };
+    }, [map, initialCenter, initialZoom]); 
+    return null;
+};
 
 
 const BuildMap = () => {
     return (
         <MapContainer
+            center = {position}
             bounds={limite}
             maxBounds={limite}
             maxBoundsViscosity={1.0}
-            style={{ height: "100%", width: "100%" }} // 100vh pour occuper toute la hauteur
+            style={{ height: "100vh", width: "100%" }} 
             zoomControl={true}
             zoom={1}
             maxZoom={3}
             minZoom={1}
-            dragging = {false}
+            //dragging = {false}
         >
             {/******************************* Chargement du plan image ******************************************************/}
             <ImageOverlay
@@ -32,7 +83,8 @@ const BuildMap = () => {
                     Blablablablabla
                 </Popup>
             </Marker>
- 
+            <ZoomControlDrag minZoom={1} maxZoom={3} />
+            <ResetViewOnZoom initialCenter={position} initialZoom={initialZoom} />
         </MapContainer>
     );
 }
--- workshop_bachelor/src/components/navbar.jsx
+++ workshop_bachelor/src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [state, setState] = useState(false);
@@ -13,7 +14,9 @@ const Navbar = () => {
         <nav className="bg-white border-b w-full md:static md:text-sm">
         <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <img src="/logo_epsi.png" width={120} height={50} alt="Epsi logo" />
+            <Link to ="/homepage">
+              <img src="/logo_epsi.png" width={120} height={50} alt="Epsi logo" />
+            </Link>
             <div className="md:hidden">
               <button
                 className="text-gray-500 hover:text-gray-800"
